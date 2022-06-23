df6ce42b2f8b6d90ecaeb481e46b354b746067c2
Nathaniel Bowers <natebowersnc@gmail.com>
2022-06-23 13:20:16 -0400

    more edits

--- scatter_net.py
+++ scatter_net.py
@@ -1,7 +1,3 @@
-'''
-    This program trains a feed-forward neural network. It takes in a geometric design (the radii of concentric spheres), and outputs the scattering spectrum. It is meant to be the first program run, to first train the weights. 
-'''
-
 # Written and tested in Python 3.10.4
 
 import tensorflow as tf
@@ -13,24 +9,39 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import RMSprop
 from tensorflow.keras.losses import MeanAbsoluteError
 from tensorflow.keras.initializers import glorot_normal
+import keras_tuner as kt
+from tensorboard.plugins.hparams import api as hp
 import os
 import time
 import argparse
-import json
 import shutil
 
+# Initialize Constants
 RANDOM_SEED = 42
+METRICS = ['accuracy', 'MeanAbsolutePercentageError']
 
+# Hyperparameters
+
+
+HP_NUM_UNITS = hp.HParam('num_units', hp.Discrete([16, 32]))
+HP_DROPOUT = hp.HParam('dropout', hp.RealInterval(0.1, 0.2))
+HP_OPTIMIZER = hp.HParam('optimizer', hp.Discrete(['adam', 'sgd']))
+
+
+
+
+# Helper Functions
 def load_data(data_path:str, percent_test=0.2):
     # creates file paths and loads the data
     path_x = data_path+"_val.csv"
     path_y = data_path+".csv"
-    data_x = np.genfromtxt(path_x,delimiter=',',dtype='float32')
-    data_y = np.transpose(np.genfromtxt(path_y,delimiter=',',dtype='float32'))
+    data_x = np.genfromtxt(path_x, delimiter=',', dtype='float32')
+    data_y = np.transpose(np.genfromtxt(path_y, delimiter=',', dtype='float32'))
     # normalizes the data (use these values to normalize any future data)
     mean, std = data_x.mean(), data_x.std()
+    stats = {'mean':float(mean), 'std':float(std)}
     data_x = (data_x-mean)/std
-    # splits the data into train/test groups
+    # splits the data into train/test groups, converts to tensors
     train_x, test_x, train_y, test_y = train_test_split(data_x, data_y,test_size=float(percent_test),random_state=RANDOM_SEED)
     val_x, test_x, val_y, test_y = train_test_split(test_x, test_y,test_size=0.5,random_state=RANDOM_SEED)
     train_x = tf.convert_to_tensor(train_x, dtype=tf.float32)
@@ -39,30 +50,7 @@ def load_data(data_path:str, percent_test=0.2):
     train_y = tf.convert_to_tensor(train_y, dtype=tf.float32)
     val_y = tf.convert_to_tensor(val_y, dtype=tf.float32)
     test_y = tf.convert_to_tensor(test_y, dtype=tf.float32)
-    return train_x, val_x, test_x, train_y, val_y, test_y, mean, std
-
-def define_model(input_shape, output_shape, lr_rate, lr_decay, end_step):
-    # metric_list provides the metrics tracked and saved
-    metric_list = ['accuracy', 'MeanSquaredError']
-    model = Sequential()
-    initializer=glorot_normal(RANDOM_SEED)
-    # add model layers
-    model.add(Dense(256, input_shape=(input_shape[1],), activation='relu', kernel_initializer=initializer))
-    model.add(Dense(256, activation='relu', kernel_initializer=initializer))
-    model.add(Dense(256, activation='relu', kernel_initializer=initializer))
-    model.add(Dense(256, activation='relu', kernel_initializer=initializer))
-    model.add(Dense(output_shape[1]))
-    model.compile(optimizer=RMSprop(learning_rate=lr_rate), loss=MeanAbsoluteError(), metrics=metric_list)
-    print(model.summary())
-    return model
-
-def evaluate_model(train_x, val_x, train_y, val_y, batch_size, num_epochs, lr_rate, lr_decay, percent_val):
-    # Assume that input and output are 1D arrays, if not, adjust as needed
-    input_shape, output_shape = train_x.shape, train_y.shape
-    end_step = np.ceil(input_shape[0]*(1-percent_val))*num_epochs
-    model = define_model(input_shape, output_shape, lr_rate, lr_decay, end_step)
-    history = model.fit(train_x, train_y, epochs=num_epochs, batch_size=batch_size, validation_data=(val_x, val_y), verbose=1)
-    return history, model
+    return train_x, val_x, test_x, train_y, val_y, test_y, stats
 
 def plot_vs_epoch(name, values, output_folder):
     path = str(output_folder + '/figures')
@@ -103,97 +91,110 @@ def set_folder(output_folder):
                 break
     return output_folder
 
-def main(data, reuse_weights, output_folder, weight_name_save, weight_name_load, batch_size, num_epochs, lr_rate, lr_decay, num_layers, n_hidden, percent_val, patienceLimit, compare, sample_val, spect_to_sample, matchSpectrum, match_test_file, designSpectrum, design_test_file):
+# Forward Training
+# rewrite with hyperparameters 
+
+
+
+def build_forward(hp):
+    model = Sequential()
+    num_layers = hp,Int('num_layers', min_value = 2, max_value = 5)
+    hp_units1 = hp.Int('units1', min_value=32, max_value=512, step=32)
+
+
+    return model
+
+
+
+
+def define_forward_model(input_shape, output_shape, lr_rate, lr_decay):
+    # metric_list provides the metrics tracked and saved
+    model = Sequential()
+    initializer=glorot_normal(RANDOM_SEED)
+    # add model layers
+    # input layer
+    model.add(Dense(256, input_shape=(input_shape[1],), activation='relu', kernel_initializer=initializer))
+    # hidden layers
+    model.add(Dense(256, activation='relu', kernel_initializer=initializer))
+    model.add(Dense(256, activation='relu', kernel_initializer=initializer))
+    model.add(Dense(256, activation='relu', kernel_initializer=initializer))
+    # output layer
+    model.add(Dense(output_shape[1]))
+    model.compile(optimizer=RMSprop(learning_rate=lr_rate), loss=MeanAbsoluteError(), metrics=METRICS)
+    print(model.summary())
+    return model
+
+def evaluate_forward(train_x, val_x, test_x, train_y, val_y, test_y, batch_size, num_epochs, lr_rate, lr_decay, percent_val):
+    # Assume that input and output are 1D arrays, if not, adjust as needed
+    input_shape, output_shape = train_x.shape, train_y.shape
+    end_step = np.ceil(input_shape[0]*(1-percent_val))*num_epochs
+    model = define_forward_model(input_shape, output_shape, lr_rate, lr_decay)
+    history = model.fit(train_x, train_y, epochs=num_epochs, batch_size=batch_size, validation_data=(val_x, val_y), verbose=1)
+    scores = model.evaluate(test_x, test_y, return_dict=True)
+    return model, history, scores
+
+def train_forward(data_path, output_folder, batch_size, num_epochs, lr_rate, lr_decay, num_layers, n_hidden, percent_val, patienceLimit):
+    # Set output folder
     # shutil.rmtree(output_folder) 
     if os.path.exists(output_folder):
         output_folder = set_folder(output_folder)
-    train_x, val_x, test_x, train_y, val_y, test_y, mean, std = load_data(data, percent_val)
+    train_x, val_x, test_x, train_y, val_y, test_y, stats = load_data(data_path, percent_val)
     print("Beginning training")
-    history, model = evaluate_model(train_x, val_x, train_y, val_y, batch_size, num_epochs, lr_rate, lr_decay, percent_val)
+    model, history, scores = evaluate_forward(train_x, val_x, test_x, train_y, val_y, test_y, batch_size, num_epochs, lr_rate, lr_decay, percent_val)
     print("Completed training")
-    # The folder model saves the data for the model
     print(model.summary())
-    model_path = os.path.join(output_folder, 'model')
-    model.save(model_path)
-    histories = history.history
-    stats = {'mean':float(mean), 'std':float(std)}
-    scores = model.evaluate(x=test_x, y=test_y, return_dict=True)
-    print(scores)
-    save_json(histories, 'histories', output_folder)
+    # Save training info 
+    model.save(os.path.join(output_folder, 'model'))
+    save_json(history.history, 'histories', output_folder)
     save_json(stats, 'stats', output_folder)
     save_json(scores, 'scores', output_folder)
-    for key in histories:
-        plot_vs_epoch(key, histories[key], output_folder)
+    for key in history.history:
+        plot_vs_epoch(key, history.history[key], output_folder)
+    return model
+
+
+
+
+
+
+
+# Predict
+def predict(model, data_x):
+    data_y = model.predict(data_x)
+    return data_y
+
+# Main
+def main(data_path, output_folder, batch_size, num_epochs, lr_rate, lr_decay, num_layers, n_hidden, percent_val, patienceLimit):
+    model_forward = train_forward(data_path, output_folder, batch_size, num_epochs, lr_rate, lr_decay, num_layers, n_hidden, percent_val, patienceLimit)
+
 
 if __name__=="__main__":
-    parser = argparse.ArgumentParser(
-        description="Physics Net Training")
-    parser.add_argument("--data",type=str,default='data/8_layer_tio2') # Where the data file is. Note: This assumes a file of _val.csv and .csv 
-    parser.add_argument("--reuse_weights",type=str,default='False') # Whether to load the weights or not. Note this just needs to be set to true, then the output folder directed to the same location. 
+    parser = argparse.ArgumentParser(description="Physics Net Training")
+    parser.add_argument("--data_path",type=str,default='data/8_layer_tio2') # Where the data file is. Note: This assumes a file of _val.csv and .csv 
     parser.add_argument("--output_folder",type=str,default='test_results') #Where to output the results to. Note: No / at the end. 
-    parser.add_argument("--weight_name_load",type=str,default="")#This would be something that goes in front of w_1.txt. This would be used in saving the weights. In most cases, just leave this as is, it will naturally take care of it. 
-    parser.add_argument("--weight_name_save",type=str,default="") #Similar to above, but for saving now. 
     parser.add_argument("--batch_size",type=int,default=64) # Batch Size
     parser.add_argument("--num_epochs",type=int,default=1000) #Max number of epochs to consider at maximum, if patience condition is not met. 
     parser.add_argument("--lr_rate",type=float,default=.001) # Learning Rate. 
     parser.add_argument("--lr_decay",type=float,default=.999) # Learning rate decay. It decays by this factor every epoch.
-    parser.add_argument("--num_layers",default=4) # Number of layers in the network. 
-    parser.add_argument("--n_hidden",default=225) # Number of neurons per layer. Fully connected layers. 
-    parser.add_argument("--percent_val",default=.2) # Amount of the data to split for validation/test. The validation/test are both split equally. 
+    parser.add_argument("--num_layers",type=int,default=4) # Number of layers in the network. 
+    parser.add_argument("--n_hidden",type=int,default=225) # Number of neurons per layer. Fully connected layers. 
+    parser.add_argument("--percent_val",type=float,default=.2) # Amount of the data to split for validation/test. The validation/test are both split equally. 
     parser.add_argument("--patience",type=int,default=10) # Patience for stopping. If validation loss has not decreased in this many steps, it will stop the training. 
-    parser.add_argument("--compare",default='False') # Whether it should output the comparison or not. 
-    parser.add_argument("--sample_val",default='True') # Wether it should sample from validation or not, for the purposes of graphing. 
-    parser.add_argument("--spect_to_sample",type=int,default=300) # Zero Indexing for this. Position in the data file to sample from (note it will take from validation)
-    parser.add_argument("--matchSpectrum",default='False') # If it should match an already existing spectrum file. 
-    parser.add_argument("--match_test_file",default='results/2_layer_tio2/test_47.5_45.3') # Location of the file with the spectrum in it. 
-    parser.add_argument("--designSpectrum",default='False') # If it should 
-    parser.add_argument("--design_test_file",default='data/test_gen_spect.csv') # This is a file that should contain 0's and 1's where it should maximize and not maximize. 
 
     args = parser.parse_args()
     dict = vars(args)
     print(dict)
-
-    for key,value in dict.items():
-        if (dict[key]=="False"):
-            dict[key] = False
-        elif dict[key]=="True":
-            dict[key] = True
-        try:
-            if dict[key].is_integer():
-                dict[key] = int(dict[key])
-            else:
-                dict[key] = float(dict[key])
-        except:
-            pass
-    # print (dict)
-
-    #Note that reuse MUST be set to true.
-    if (dict['compare'] or dict['matchSpectrum'] or dict['designSpectrum']):
-        if dict['reuse_weights'] != True:
-            print("Reuse weights must be set true for comparison, matching, or designing. Setting it to true....")
-            time.sleep(1)
-        dict['reuse_weights'] = True
         
     kwargs = {  
-            'data':dict['data'],
-            'reuse_weights':dict['reuse_weights'],
+            'data_path':dict['data_path'],
             'output_folder':dict['output_folder'],
-            'weight_name_save':dict['weight_name_save'],
-            'weight_name_load':dict['weight_name_load'],
             'batch_size':dict['batch_size'],
             'num_epochs':dict['num_epochs'],
             'lr_rate':dict['lr_rate'],
             'lr_decay':dict['lr_decay'],
-            'num_layers':int(dict['num_layers']),
-            'n_hidden':int(dict['n_hidden']),
+            'num_layers':dict['num_layers'],
+            'n_hidden':dict['n_hidden'],
             'percent_val':dict['percent_val'],
             'patienceLimit':dict['patience'],
-            'compare':dict['compare'],
-            'sample_val':dict['sample_val'],
-            'spect_to_sample':dict['spect_to_sample'],
-            'matchSpectrum':dict['matchSpectrum'],
-            'match_test_file':dict['match_test_file'],
-            'designSpectrum':dict['designSpectrum'],
-            'design_test_file':dict['design_test_file']
             }
     main(**kwargs)
